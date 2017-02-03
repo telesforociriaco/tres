0eda383bbc46bdac0ed6fb7d5153a19c30d34722
miguelescanero <miguel.escanero1@gmail.com>
2017-02-02 20:06:12 -0600

    estilosssss

---
+++ calculadora.js
@@ -0,0 +1,33 @@
+
+function suma(){
+  a = document.getElementById('numero1').value;
+  b = document.getElementById('numero2').value;
+  resultado =a+b;
+  return resultado;
+  alert(resultado);
+}
+
+function resta(a,b){
+  a = document.getElementById('numero1').value;
+  b = document.getElementById('numero2').value;
+  resultado =a-b;
+  return resultado;
+  alert(resultado);
+}
+
+function multiplicacion(a,b){
+  a = document.getElementById('numero1').value;
+  b = document.getElementById('numero2').value;
+  resultado = a * b;
+  return resultado;
+  alert(resultado);
+}
+
+function divicion(a,b){
+  a = document.getElementById('numero1').value;
+  b = document.getElementById('numero2').value;
+  resultado = a/b;
+  return resultado;
+  alert(resultado);
+}
+alert("si pero no te enojes");
