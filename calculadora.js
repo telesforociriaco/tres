c9c5cb5e78bf22685ac42cadb4f7ac7c78319058
felrivglz <felrivglz@gmail.com>
2017-02-02 19:35:59 -0600

    te envio las funciones  telesforo

---
+++ calculadora.js
@@ -0,0 +1,30 @@
+
+
+
+function suma(){
+  a = document.getElementById('numero1').value;
+  b= document.getElementById('numero2').value;
+  resultado =a+b;
+  return resultado;
+}
+
+function resta(a,b){
+  a = document.getElementById('numero1').value;
+  b= document.getElementById('numero2').value;
+  resultado =a-b;
+  return resultado;
+}
+
+function multiplicacion(a,b){
+  a = document.getElementById('numero1').value;
+  b= document.getElementById('numero2').value;
+  resultado =a*b;
+  return resultado;
+}
+
+function divicion(a,b){
+  a = document.getElementById('numero1').value;
+  b= document.getElementById('numero2').value;
+  resultado =a/b;
+  return resultado;
+}
