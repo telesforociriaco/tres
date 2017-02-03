9c6aa59e92820f29cb5e433928388cb1d6126341
felrivglz <felrivglz@gmail.com>
2017-02-02 19:49:33 -0600

    saludos alerta para sergio

--- calculadora.js
+++ calculadora.js
@@ -1,30 +1,29 @@
 
-
-
 function suma(){
   a = document.getElementById('numero1').value;
-  b= document.getElementById('numero2').value;
+  b = document.getElementById('numero2').value;
   resultado =a+b;
   return resultado;
 }
 
 function resta(a,b){
   a = document.getElementById('numero1').value;
-  b= document.getElementById('numero2').value;
+  b = document.getElementById('numero2').value;
   resultado =a-b;
   return resultado;
 }
 
 function multiplicacion(a,b){
   a = document.getElementById('numero1').value;
-  b= document.getElementById('numero2').value;
-  resultado =a*b;
+  b = document.getElementById('numero2').value;
+  resultado = a * b;
   return resultado;
 }
 
 function divicion(a,b){
   a = document.getElementById('numero1').value;
-  b= document.getElementById('numero2').value;
-  resultado =a/b;
+  b = document.getElementById('numero2').value;
+  resultado = a/b;
   return resultado;
 }
+alert("si pero no te enojes");
