1b7991ecf3f5596a09a9f4103c4ca46613e49e6e
felrivglz <felrivglz@gmail.com>
2017-02-02 20:25:52 -0600

    comente los repas

--- calculadora.js
+++ calculadora.js
@@ -2,32 +2,37 @@
 function suma(){
   a = document.getElementById('numero1').value;
   b = document.getElementById('numero2').value;
-  resultado =a+b;
+//  alert(typeof(a));
+//  resultado = number(a)+number(b);
+    alert(resultado);
   return resultado;
-  alert(resultado);
+
 }
 
 function resta(a,b){
   a = document.getElementById('numero1').value;
   b = document.getElementById('numero2').value;
-  resultado =a-b;
+  resultado = a-b;
+    alert(resultado);
   return resultado;
-  alert(resultado);
+
 }
 
 function multiplicacion(a,b){
   a = document.getElementById('numero1').value;
   b = document.getElementById('numero2').value;
-  resultado = a * b;
-  return resultado;
+  resultado = a*b;
   alert(resultado);
+  return resultado;
+
 }
 
 function divicion(a,b){
   a = document.getElementById('numero1').value;
   b = document.getElementById('numero2').value;
   resultado = a/b;
+    alert(resultado);
   return resultado;
-  alert(resultado);
+
 }
 alert("si pero no te enojes");
